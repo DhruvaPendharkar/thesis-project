91a4e1b4e178e712496d99d33d5ec3aebc6be8e8
DhruvaPendharkar <dhruva.pendharkar@gmail.com>
2018-03-01 23:29:41 -0600

    Adding Story Tests

---
+++ Approach2/Comprehension Passages/test/JacksonvilleFloridaStoryTest.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+
+/**
+ * Created by dhruv on 2/19/2018.
+ */
+public class JacksonvilleFloridaStoryTest {
+    @BeforeAll
+    static void TestSetupParser() {
+        Sentence.SetupLexicalizedParser();
+        Assert.assertNotNull(Sentence.parser);
+        Assert.assertNotNull(Sentence.pipeline);
+    }
+
+    @BeforeEach
+    void InitializeTest(){
+        Word.eventId = 1;
+    }
+
+    @Test
+    //
+    void TestSentenceOne() {
+        String content = "";
+        Sentence sentence = Sentence.ParseSentence(content);
+        System.out.println(Sentence.DependenciesToString(sentence));
+        List<Rule> rules = sentence.GenerateRules();
+        HashSet<String> ruleString = new HashSet<>();
+        TreeSet<Rule> rulesSet = new TreeSet<>(rules);
+        for(Rule rule : rulesSet){
+            ruleString.add(rule.toString());
+            System.out.println(rule.toString());
+        }
+
+        for(Rule rule : rulesSet){
+            System.out.println(String.format("Assert.assertTrue(ruleString.contains(\"%s\"));", rule.toString()));
+        }
+
+        Assert.assertEquals(0, ruleString.size());
+    }
+}
